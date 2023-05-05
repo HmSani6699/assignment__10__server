2c0e48ec9aaa784bc154879bed9a37d60e8408e0
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-05 20:08:28 +0600

    update to tada

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const cors= require('cors')
 const port = 5000
+
+const user= require('./data/userData.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/user',(req,res)=>{
+  res.send(user)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
