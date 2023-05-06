26223f0debfb0091e0db652501ac2b39b9c12a42
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-06 22:49:15 +0600

    added to dainamic data load

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const cors= require('cors')
+const cors = require('cors')
 const port = 5000
 
-const user= require('./data/userData.json')
+const user = require('./data/userData.json')
 
 app.use(cors())
 
@@ -11,10 +11,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/user',(req,res)=>{
+app.get('/user', (req, res) => {
   res.send(user)
 })
 
+app.get('/user/:id', (req, res) => {
+  const id = req.params.id;
+  const singleUser = user.find(u => u.id==id);
+  res.send(singleUser)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
